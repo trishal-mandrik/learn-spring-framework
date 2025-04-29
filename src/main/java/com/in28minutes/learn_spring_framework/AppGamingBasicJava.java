92731d86c1062e8c6aeac600805a949683c9f7dc
trishalmandrik <trishal.mandrik@springernature.com>
2025-04-29 23:39:14 +0530

    Iteration-1

---
+++ src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
@@ -0,0 +1,14 @@
+package com.in28minutes.learn_spring_framework;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.MarioGame;
+import com.in28minutes.learn_spring_framework.game.SuperContraGame;
+
+public class AppGamingBasicJava {
+    public static void main(String[] args) {
+//        var marioGame = new MarioGame();
+        var superContraGame = new SuperContraGame();
+        var gameRunner = new GameRunner(superContraGame);
+        gameRunner.run();
+    }
+}
---
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -0,0 +1,16 @@
+package com.in28minutes.learn_spring_framework.game;
+
+public class GameRunner {
+    SuperContraGame game;
+    public GameRunner(SuperContraGame game) {
+        this.game = game;
+    }
+
+    public void run() {
+        System.out.println("Running game: " + game);
+        game.up();
+        game.down();
+        game.left();
+        game.right();
+    }
+}
