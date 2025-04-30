a0fa2efbf5c2a8de5677766bade8c1c55be27bd1
trishalmandrik <trishal.mandrik@springernature.com>
2025-04-30 23:35:34 +0530

    Iteration-2

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -4,6 +4,8 @@ import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
         // Launch a spring context -
@@ -13,7 +15,11 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("address2"));
         System.out.println(context.getBean(Address.class));
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,9 +1,11 @@
 package com.in28minutes.learn_spring_framework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-record Person(String name, int age){}
+record Person(String name, int age, Address address) {}
 
 //Address - firstLine & city
 record Address(String firstLine, String city){}
@@ -23,11 +25,33 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("John Doe", 20);
+        return new Person("John Doe", 20, new Address("Kasaridih", "Durg"));
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address2) {
+        return new Person(name, age, address2);
+    }
+
+    @Bean
+    public Person person4Parameters(String name, int age, Address address) {
+        return new Person(name, age, address);
     }
 
     @Bean(name = "address2")
+    @Primary
     public Address address() {
         return new Address("Baker street", "London");
+    }
+
+    @Bean(name = "address3")
+    @Qualifier("address3Qualifier")
+    public Address address3() {
+        return new Address("Baker street", "London");
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
     }
 }
